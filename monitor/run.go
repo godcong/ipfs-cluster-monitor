989bf7c82813e7c00c45d814831b6cef87486a78
godcong <jumbycc@163.com>
2018-12-28 04:04:06 +0800

    add cluster status

--- monitor/run.go
+++ monitor/run.go
@@ -1,11 +1,21 @@
 package monitor
 
+import "context"
+
 type ClusterStatus struct {
 	//ipfs info
 	//service info
 }
 
 // StartRun ...
-func StartRun() {
-
+func StartRun(ctx context.Context) {
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			}
+			//get info
+		}
+	}()
 }
