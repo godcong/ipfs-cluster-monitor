3d003873394e8a4e4466e8f464796b4eb981b6ae
godcong <jumbycc@163.com>
2019-01-29 14:54:51 +0800

    update comment

--- main.go
+++ main.go
@@ -1,31 +1,55 @@
+//go:generate protoc --go_out=plugins=grpc:./proto monitor.proto
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/godcong/ipfs-cluster-monitor/api"
-	"github.com/godcong/ipfs-cluster-monitor/cluster"
-	"github.com/juju/errors"
+	"github.com/godcong/ipfs-cluster-monitor/config"
+	"github.com/godcong/ipfs-cluster-monitor/service"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 )
 
+var configPath = flag.String("config", "config.toml", "config path")
+
 func main() {
-	cluster.LogInit()
+	flag.Parse()
 
-	engine := gin.Default()
-	engine.NoRoute(NoResponse)
-
-	c := cluster.Default()
-	go c.Start()
-	defer c.Stop()
-
-	api.Router(engine)
-	err := engine.Run(cluster.Config().RemotePort)
+	flag.Parse()
+	file, err := os.OpenFile("monitor.log", os.O_SYNC|os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
 	if err != nil {
-		errors.ErrorStack(err)
+		panic(err)
 	}
+
+	log.SetOutput(io.MultiWriter(file, os.Stdout))
+	log.SetFlags(log.Ldate | log.Lshortfile)
+
+	err = config.Initialize(*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	//start
+	service.Start()
+
+	go func() {
+		sig := <-sigs
+		fmt.Println(sig, "exiting")
+		service.Stop()
+		done <- true
+	}()
+	<-done
+
 }
 
 // NoResponse ...
